46e0a419f4eb58ef7f6c44f6b61887d377b01e73
keita <dmwwvn@gmail.com>
2024-06-30 01:17:14 +0900

    20240630追加

---
+++ project/13.WordQuiz/13-3.WordQuiz/main.js
@@ -0,0 +1,81 @@
+class WordQuiz {
+  constructor(rootElm) {
+    this.rootElm = rootElm;
+  }
+
+  async init() {
+    await this.fetchQuizData();
+    this.displayStartView();
+  }
+
+  async fetchQuizData() {
+    try {
+      const response = await fetch("quiz.json");
+      this.quizData = await response.json();
+    } catch (e) {
+      this.rootElm.innerText = "問題の読み込みに失敗しました。";
+      console.log(e);
+    }
+  }
+
+  displayStartView() {
+    const levelStrs = Object.keys(this.quizData);
+    const optionStrs = [];
+    for (let i = 0; levelStrs.length > i; i++) {
+      optionStrs.push(
+        `<option value="${levelStrs[i]}" name="level">レベル${i + 1}</option>`
+      );
+    }
+    const html = `<select class="levelSelector">${optionStrs.join("")}</select>
+    <button class="startBtn">スタート</button>
+    `;
+    const parentElm = document.createElement("div");
+    parentElm.innerHTML = html;
+
+    const startBtnElm = parentElm.querySelector(".startBtn");
+    startBtnElm.addEventListener("click", () => {
+      this.displayQuestionView();
+    });
+
+    this.replaceView(parentElm);
+  }
+
+  displayQuestionView() {
+    const html = `<p>ゲームを開始しました</p>
+    <button class="retireBtn">ゲームを終了する</button>`;
+
+    const parentElm = document.createElement("div");
+    parentElm.className = "question";
+    parentElm.innerHTML = html;
+
+    const retireBtnElm = parentElm.querySelector(".retireBtn");
+    retireBtnElm.addEventListener("click", () => {
+      this.displayResultView();
+    });
+
+    this.replaceView(parentElm);
+  }
+
+  displayResultView() {
+    const html = `<p>ゲーム終了</p>
+    <button class="resetBtn">開始画面に戻る</button>`;
+
+    const parentElm = document.createElement("div");
+    parentElm.className = "results";
+    parentElm.innerHTML = html;
+
+    const resetBtnElm = parentElm.querySelector(".resetBtn");
+    resetBtnElm.addEventListener("click", () => {
+      this.displayStartView();
+    });
+
+    this.replaceView(parentElm);
+  }
+
+  replaceView(elm) {
+    this.rootElm.innerHTML = "";
+    this.rootElm.appendChild(elm);
+  }
+}
+
+new WordQuiz(document.getElementById("app")).init();
